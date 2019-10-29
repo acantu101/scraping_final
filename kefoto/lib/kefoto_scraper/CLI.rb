61d63f426fff559645bcbc9e5caa19cd5785c4c6
Alejandra Cantu <alejandrac@fb.com>
2019-10-29 14:26:49 -0500

    Update CLI.rb

--- kefoto/lib/kefoto_scraper/CLI.rb
+++ kefoto/lib/kefoto_scraper/CLI.rb
@@ -11,24 +11,18 @@
       def initialize
       @page_url = "https://www.kefotos.mx/"
       @product_names = get_product_names()
-      @price_range = []
       end
 
       def call
         puts "These are the services that Kefoto offers:"
         get_product_names
         list_products
-        puts "Enter the number of the product you wish to inspect"
-        @answer = gets.chomp
-        puts "Selecting #{@answer}. #{@product_names[@answer.to_i-1]}"
-
-        select_from_list
+        select_from_list_cli
         price_array
-
-        if @answer == "1"
-          photo_enmarcada_price_range_cli
-        else
-          price_cli
+          if @answer == "1"
+            photo_enmarcada_price_range_cli
+          else
+            price_cli
         end
       end
 
@@ -55,24 +49,38 @@
           end
       end
 
-      def select_from_list
+      def select_from_list_cli
+        puts "Enter the number of the product you wish to inspect"
+        @answer = gets.chomp
+
+        if @answer.to_i.between?(1,5) == false
+          puts "You did not select a valid number from the list. Please try again."
+          call
+        else
+        puts "Selecting #{@answer}. #{@product_names[@answer.to_i-1]}"
         @service_links = get_html.css(".nav-link").map {|link| link['href']}
         @selection = @service_links[@answer.to_i-1]
         @url = @page_url.concat(@selection)
         @product_url = Nokogiri::HTML(HTTParty.get @url)
+        end
       end
+
+
 
       def price_array
         @price_array = []
         @product_prices = @product_url.css(".container-fluid").text
         @prices = @product_prices.scan(/[\$£](\d{1,3}(,\d{3})*(\.\d*)?)/)
         @prices.each do |price|
-          @price_array << price[0]
-
+          if @price_array != price
+            @price_array << price[0]
         end
+        @price_array
+      end
       end
 
       def photo_enmarcada_price_range_cli
+          @price_range = @price_array.uniq
           @price_range_size_dimension = @price_range.take(6)
           @price_range_frame_type = @price_range.drop(6)
           puts "These are the range of prices based on dimension:"
@@ -88,7 +96,6 @@
           puts "Based on frame type:"
           puts @price_range_frame_type.uniq
           main_menu
-
           elsif @answer_1 == "n"
           main_menu
         end
@@ -96,17 +103,13 @@
       end
 
       def price_cli
-          puts "These are the range of prices"
-          puts "#{@price_array.min} - #{@price_array.max}"
-          puts "Would you like to see the uniq prices in the range for both of these? y/n"
-          @answer_1 = gets.chomp
+          puts "These are the prices"
+          puts @price_array
+          main_menu
+      end
 
-          if @answer_1 == "y"
-              puts @price_array.uniq
-              main_menu
-          elsif @answer_1 == "n"
-              main_menu
-          end
+      def clear_price
+        @price_array.clear
       end
 
 
@@ -114,7 +117,7 @@
           puts "Do you want to return to the main menu? y/n"
           @yn = gets.chomp
         if @yn ==  "y"
-          call
+          KefotoScraper::CLI.new.call
         end
       end
 
